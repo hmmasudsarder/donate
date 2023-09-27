8a3d455b93ac68f1997b576bc21290db538e34ae
hmdmasudsarder <hmmasudsarder@gmail.com>
2023-09-27 22:54:05 +0600

    statick commit

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,39 +1,50 @@
-import { useEffect, useState } from "react";
-import { Pie, PieChart, ResponsiveContainer } from "recharts";
 
+import { PieChart, Pie,  Cell } from 'recharts';
 
-
-const Statistics = () => {
-    const [data, setData] = useState([]);
-const {price } = data;
-useEffect(() => {
-    fetch('/donation.json')
-    .then(res => res.json())
-    .then(data => setData(data))
-}, []);
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
     return (
-        <div>
-           <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+const Statistics = () => {
+  const donationData = localStorage.getItem('donations');
+  const dataFull = JSON.parse(donationData)?.length || 0;
+  console.log(dataFull);
+  const data = [
+    { name: 'Your Donation', value:  dataFull},
+    { name: 'TotalDonation', value: 12 - dataFull},
+    
+  ];
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      
+      <PieChart width={400} height={400}>
           <Pie
-
             data={data}
             cx="50%"
             cy="50%"
             labelLine={false}
-            // label={renderCustomizedLabel}
+            label={renderCustomizedLabel}
             outerRadius={80}
             fill="#8884d8"
             dataKey="value"
           >
-            {/* {data.map((entry, index) => (
+            {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))} */}
+            ))}
           </Pie>
         </PieChart>
-      </ResponsiveContainer>
-        </div>
-    );
+        
+    </div>
+  );
 };
 
 export default Statistics;
